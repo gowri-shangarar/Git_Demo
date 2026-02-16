4e9cd69dd23b33b61059b4a24db57ef29a529dad
Malini-Ganesan <MaliniGanesan@d3minds.com>
2026-02-16 17:31:20 +0530

    changes made

--- demo.py
+++ demo.py
@@ -26,24 +26,24 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid data. Please enter a number.")
             continue
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '4':
+        elif choice == '5':
             print(num1, "/", num2, "=", divide(num1, num2))
 
-         elif choice == '2':
+         elif choice == '3':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
+        elif choice == '2':
             print(num1, "*", num2, "=", multiply(num1, num2))
         
         # check if user wants another calculation
