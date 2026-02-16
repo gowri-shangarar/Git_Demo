b48e01dd6cf52eb38475870c3febbc88311b363d
Suganraj-D3 <Suganraj.K@d3minds.com>
2026-02-16 16:41:01 +0530

    python file changes

--- demo.py
+++ demo.py
@@ -15,11 +15,11 @@ def divide(x, y):
     return x / y
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+# print("Select operation.")
+# print("1.Add")
+# print("2.Subtract")
+# print("3.Multiply")
+# print("4.Divide")
 
 while True:
     # take input from the user
