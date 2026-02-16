60fd38aa4671c4f9dfdce9b7184d575ad6804a83
Malini-Ganesan <MaliniGanesan@d3minds.com>
2026-02-16 17:38:24 +0530

    changes made

--- demo.py
+++ demo.py
@@ -19,14 +19,13 @@ print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Divide")
-print("4.Multiply")
 
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '5'):
+    if choice in ('1', '2', '3'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -37,9 +36,6 @@ while True:
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '5':
-            print(num1, "/", num2, "=", divide(num1, num2))
-
          elif choice == '3':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
@@ -48,7 +44,7 @@ while True:
         
         # check if user wants another calculation
         # break the while loop if answer is no
-        # next_calculation = input("Let's do next calculation? (yes/no): ")
+        next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
     else:
